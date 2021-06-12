9142f2bd89a48dfb6546342ce6e0836c5c204c98
RonaldColyar <phantexcreations@gmail.com>
2021-06-11 21:31:35 -0500

    progression

--- frontend/src/Components/AverageHumidity.tsx
+++ frontend/src/Components/AverageHumidity.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import humidity from "../Img/humidity.png";
 export default class AverageHumidity extends Component {
     render() {
         return (
             <div id = "average-humidity"  className = "average-stats">
                 <h1>Average Humidity</h1>
+                <img src = {humidity}></img>
+                <h2>12</h2>
             </div>
         )
     }
--- frontend/src/Components/NutrientTemp.tsx
+++ frontend/src/Components/NutrientTemp.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-
+import nutrients from "../Img/nutrients.png";
 export default class NutrientTemp extends Component {
     render() {
         return (
             <div  id= "average-nutrient"className = "average-stats">
                 <h1>Average Nutrient Temp</h1>
-                
+                <img src = {nutrients}></img>
+                <h2>10</h2>
             </div>
         )
     }
